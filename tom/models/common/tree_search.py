fixing bug in model based loss calculation

tom/models/common/tree_search.py
@@ -1,5 +1,53 @@
+import torch as th 
+import numpy as np
 
 
+def best_first_search(policy_net, transition_net, obs, mask, args):
+    node_obs = obs
+    q_estimate = None 
+    current_q = 0
+    current_gamma = args.gamma 
+    samples = 0
+    at_node = True 
+    sampled_action = None 
+    with th.no_grad(): original_q_values = -(policy_net(th.from_numpy(obs).to(args.device)).cpu().numpy() * -mask)
+    q_estimate = [[] for _ in original_q_values]
 
-def best_first_search():
-    return 
+    while True:
+        samples += 1
+        if(samples > args.planning_depth): break 
+        with th.no_grad(): q_values = -(policy_net(th.from_numpy(obs).to(args.device)).cpu().numpy() * -mask)
+
+        # Predict action based on a soft-max top k q_values 
+        top_k_ind = np.argpartition(q_values, -10)[-10:]
+        top_k_q = q_values[top_k_ind]
+        soft_sample = np.exp(top_k_q) / np.sum(np.exp(top_k_q))
+        action_index = np.random.choice(top_k_ind, 1, p=soft_sample)[0]
+        action = np.zeros_like(q_values)
+        action[action_index] = 1
+
+        transition_in = np.concatenate((obs, action))
+        with th.no_grad(): trans_out = transition_net(th.from_numpy(transition_in).float().to(args.device)).cpu().numpy()
+        done = trans_out[-2]
+        mask = trans_out[len(obs[0]):-2]
+        obs = trans_out[:obs.shape[0]]
+        reward = trans_out[-1]
+
+        current_q += current_gamma * reward
+        current_gamma *= args.gamma 
+
+        if(at_node):
+            at_node = False
+            sampled_action = action_index 
+        
+        done = 1 if np.random.random_sample() > done else 0 # clip done to 0 or 1 
+        if(done):
+            q_estimate[sampled_action].append(current_q) 
+            obs = node_obs
+            at_node = True 
+            current_gamma = args.gamma 
+            current_q = 0
+
+    q_estimate = np.asarray([0 if len(arr) == 0 else np.mean(np.asarray(arr)) for arr in q_estimate])
+    action = np.argmax(q_estimate)
+    return action 

train.py
@@ -15,21 +15,23 @@ os.environ["CUDA_LAUNCH_BLOCKING"] = "1"
 parser = argparse.ArgumentParser(description='Options for Multi-Agent Theory of Mind Model.')
 parser.add_argument('--model_type', default = 'random', type=str, help='full, attention, belief, dqn')
 parser.add_argument('--player_id', default = 0, type=int, help='ID of player for compare runs')
-parser.add_argument('--timesteps',  default = 1000000, type=int, help='Number of timesteps to run')
+parser.add_argument('--timesteps',  default = 100000, type=int, help='Number of timesteps to run')
 parser.add_argument('--log_time', default = 10000, type=int, help="Frequency to log model")
 parser.add_argument('--layers', default=[64,64], type=List,  help='Layer structure for NNs')
 parser.add_argument('--compare', default=False, dest='compare', action='store_true')
-parser.add_argument('--load_folder', default='./trained_models/models/', type=str,  help='Model to save folders')
-parser.add_argument('--save_folder', default='./trained_models/models/', type=str,  help='Model to load folders')
-parser.add_argument('--opponent', default='./trained_models/models/', type=str,  help='Model to load opponent from or string opponent type')
+parser.add_argument('--compare_folder', default='./', type=str,  help='Model to save model comparison results')
+parser.add_argument('--load_folder', default='./', type=str,  help='Model to save folders')
+parser.add_argument('--save_folder', default='./trained_models/models/test/', type=str,  help='Model to load folders')
+parser.add_argument('--opponent', default='./', type=str,  help='Model to load opponent from or string opponent type')
 parser.add_argument('--batch_size', default=256, type=int,  help='Model to save folders')
 parser.add_argument('--device', default="cuda", type=str,  help='Model to save folders')
-parser.add_argument('--gamma', default=0.999, type=int,  help='Model to save folders')
+parser.add_argument('--gamma', default=0.95, type=int,  help='Model to save folders')
 parser.add_argument('--target_update', default=10, type=int,  help='Target update network frequency')
-parser.add_argument('--planning', default=True, type=bool,  help='Model based or model free')
+parser.add_argument('--model_based', default=False, dest='model_based', action='store_true')
+parser.add_argument('--planning_depth', default=1000, type=int, help="number of samples to run")
 parser.add_argument('--base_model', default='', type=str,  help='Base model for planning')
 
-# python .\train.py --compare --load_folder ./trained_models/texas_holdem/DQN_10M/models-100/ --opponent ./trained_models/texas_holdem/DQN_1M/models-100/ 
+# python .\train.py --save_folder ./trained_models/liarsdice/DPN_1M/ --timesteps 1000000 --log_time 10000
 
 args = parser.parse_args()
 
@@ -39,12 +41,13 @@ model = MADQN(env=env, args=args)
 
 if(args.compare):
     data = model.compare(args)
+    path = os.path.abspath(os.path.join(args.compare_folder , args.load_folder + "p" + str(args.player_id) , args.opponent))
+    if(not os.path.exists(path)): os.makedirs(path)
+    np.save(path +  "/results.npy", data)
 else:
     data = model.learn(total_timesteps = args.timesteps)
-    model.save(args.save_folder, "models-100")
+    model.save(args.save_folder, "models-" + str(int(args.timesteps / args.log_time)))
 
-print("episode reward mean: ", np.mean(np.abs(data)))
-print("total episode reward: ", np.sum(data, axis=0))
 data = np.array(data)
 model_data = data[:, args.player_id]
 model_wins = np.count_nonzero(model_data > 0)